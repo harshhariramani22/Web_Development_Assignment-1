4b25c934c69dd727891fae0795125725e888e4b2
hariramani <hr951414@dal.ca>
2022-06-06 21:56:15 -0300

    Updated some files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
 import './App.css';
-
+import ResponsiveAppBar from './Navbar';
+import CustForm from './customerform';
+import NameForm from './customerform';
+import MenuAppBar from './Navbar';
+import MyForm from './customerform';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div>
+      <MenuAppBar/>
+    </div>
+    <div >
+        <MyForm/>
+    </div>
+  
     </div>
   );
 }
